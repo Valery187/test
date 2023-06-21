b1503eb04191068d2d586c12eee5df149dcc0adf
Valery187 <lazarv187@yandex.ru>
2023-06-21 11:25:20 +0400

    Проверка палиндрома

--- main.py
+++ main.py
@@ -27,13 +27,34 @@
 # strconter('aabccdcd')  # буквы выводятся по одному разу
 
 # Оптимизация кода с помощью словаря
-def strconter(s):
-    syms_counter = {}
-    for symbol in s:
-        syms_counter[symbol] = syms_counter.get(symbol, 0) + 1
+# def strconter(s):
+#     syms_counter = {}
+#     for symbol in s:
+#         syms_counter[symbol] = syms_counter.get(symbol, 0) + 1
+#
+#     for symbol, count in syms_counter.items():
+#         print (symbol, count)
+#
+# # strconter('aabccdcd')
+# strconter('hello word')
 
-    for symbol, count in syms_counter.items():
-        print (symbol, count)
+# Задача 4
+# На вход подается строка, все символы находятся в нижнем регистре и без пробелов. Напишите функцию, которая будет возвращать True, если строка является палиндромом и False, если строка палиндромом не является.
+# Примечание: палиндром — это строка, которая читается одинаково как слева направо, так и справа налево
+# Пример входных данных 1: лепсспел
+# Пример выходных данных 1: True
+# Пример входных данных 2: helloworld
+# Пример выходных данных 2: False
 
-# strconter('aabccdcd')
-strconter('hello word')
+def Revers(s):
+    revers_s = ''
+    for i in range(len(s) - 1, -1, -1):
+        revers_s += s[i]
+    if revers_s == s:
+        print('True')
+    else:
+        print('False')
+
+
+Revers('лепсспел')
+Revers('helloworld')
